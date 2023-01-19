60642182ed9f8a799b9257a0ca381e3cdb126d2a
mtn050 <106604563+mtn050@users.noreply.github.com>
2023-01-19 13:46:03 -0300

    l

--- src/Components/CardWidget/CardWidget.js
+++ src/Components/CardWidget/CardWidget.js
@@ -7,7 +7,7 @@ import './CardWidget.css'
 /*---------Logica-----------*/
 const CardWidget = (props) =>{
     return(
-        <header class = "CardW">
+        <header>
             <img src="https://images.vexels.com/media/users/3/200097/isolated/preview/942820836246f08c2d6be20a45a84139-icono-de-carrito-de-compras-carrito-de-compras.png" 
              alt="img not found" class="numero"/>{props.cantidad}
         </header>
